2d4d6124cc553e855602d25fe464f63816001d2e
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-10-18 00:05:00 -0300

    refactor: add author required in posts resources

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -75,7 +75,7 @@ class PostResource extends Resource
                         SpatieMediaLibraryFileUpload::make('profilePic')
                             ->label('Profile pic')->disk('s3')->responsiveImages()
                             ->visibility('public')->directory('authors_profile_pic')->image(),
-                    ])
+                    ])->required()
                     ->preload(),
 
                 TinyEditor::make('content')->showMenuBar()->language('es')->toolbarSticky(true)->columnSpan('full')->fileAttachmentsDisk('s3')->fileAttachmentsVisibility('public')->fileAttachmentsDirectory('posts_content')->maxWidth("740px")->required(),
