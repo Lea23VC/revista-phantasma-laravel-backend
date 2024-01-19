03934baf4909a47ccdf20625c9e71675af5dfd3e
Lea23VC <leandro.villalobos.dx@gmail.com>
2024-01-18 21:32:50 -0300

    refactor: now telescope is disable for most things outside local

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -20,7 +20,9 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            return true;
+            if ($this->app->environment('local')) {
+                return true;
+            }
 
             return $entry->isReportableException() ||
                 $entry->isFailedRequest() ||
