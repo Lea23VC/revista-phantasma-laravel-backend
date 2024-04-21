0cb5a5a0fcda62c45b8653e2944f231d1bb73e89
Lea23VC <leandro.villalobos.dx@gmail.com>
2024-04-21 01:23:00 -0400

    refactor: change position height

--- resources/views/forms/components/image-position-preview.blade.php
+++ resources/views/forms/components/image-position-preview.blade.php
@@ -3,7 +3,7 @@
         <!-- Interact with the `state` property in Alpine.js -->
         @if (isset($getState()['featuredImage']))
         <div class="aspect-square md:aspect-auto overflow-hidden group">
-            <div class="relative p-6" style="height: {{ $getState()['type'] == 'preview' ? '448px' : '250px' }}">
+            <div class="relative p-6" style="height: {{ $getState()['type'] == 'preview' ? '448px' : '180px' }}">
                 <img src=" {{ $getState()['featuredImage'] }}"
                     style="object-position: {{ str_replace('-', ' ',$getState()['imagePosition']) }};"
                     alt="Image preview"
