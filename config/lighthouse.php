f4fe9f9f860ced3691932a9dbaf53797d6ce2410
Lea23VC <leandro.villalobos.dx@gmail.com>
2024-04-21 17:14:47 -0400

    refactor: performance

--- config/lighthouse.php
+++ config/lighthouse.php
@@ -92,8 +92,7 @@ return [
         /*
          * Setting to true enables schema caching.
          */
-        // 'enable' => env('LIGHTHOUSE_SCHEMA_CACHE_ENABLE', env('APP_ENV') !== 'local'),
-        'enable' => false,
+        'enable' => env('LIGHTHOUSE_SCHEMA_CACHE_ENABLE', env('APP_ENV') !== 'local'),
 
         /*
          * File path to store the lighthouse schema.
